d9138b81b011cc7640d5f73336472f0874bbea6e
Ikramsrr <33417689+Ikramsrr@users.noreply.github.com>
2017-11-19 09:34:48 +0100

    Create coeur.h

---
+++ coeur.h
@@ -0,0 +1,15 @@
+#ifndef COEUR_H_INCLUDED
+#define COEUR_H_INCLUDED
+#pragma once      
+
+void une_led_sur_deux();
+void une_led_sur_trois();
+void chenille();
+void chenille2();
+void deux_chenille();
+void toutes_les_leds();
+void au_choix(int x);
+void optionelle1();
+void optionelle2();
+
+#endif // COEUR_H_INCLUDED
