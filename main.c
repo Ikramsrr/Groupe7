5bd5fb283947ca5c08bdc21f3a07f11f3eda7fe6
Ikramsrr <33417689+Ikramsrr@users.noreply.github.com>
2017-11-19 09:48:27 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "donnee.h"
+#include "action.h"
+#include "menu.h"
+
+
+int main ()
+{ int   n2=0 , n=0  ;  
+ battm T[500] ;
+  int tab[500] ;
+  int menu ; 
+  menu = affichage_menu ();
+  execution (&tab,&n);
+  manipulation (tab,T,n ,&n2 );
+  affichage_particulier (T,n2 );
+  
+ 
+	return 0;
+}
